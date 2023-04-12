b139b51716c65cace6da2c66eaa6bd6254110642
RASHID ALI BAKARI <rashidestonian@gmail.com>
2023-04-12 17:23:30 +0300

    Flask app

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
+@app.route("/index")
 def index():
-    return "Hello world"
+    user = {"name":"Rashid"}
+    posts = [
+        {
+            "author":{"name":"Susan"},
+            "body": "Hello, there. My name is susan and today we are going to talk about Tech."
+        },
+        {
+            "author":{"name":"Zack"},
+            "body": "Hello, there. My name is Zack and today we are going to talk about Economics."
+        }
+
+    ]    
+    return render_template("index.html", user=user, title="Home", posts=posts)
